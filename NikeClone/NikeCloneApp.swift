27cc7c037c15d1c12f8c176b6f7edc75acf08057
Nabraj Khadka <nabrajkhadka43@gmail.com>
2024-06-03 10:59:22 +0545

    Initial Commit

---
+++ NikeClone/NikeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeCloneApp.swift
+//  NikeClone
+//
+//  Created by nex on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct NikeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NikeCloneUITests/NikeCloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NikeCloneUITestsLaunchTests.swift
+//  NikeCloneUITests
+//
+//  Created by nex on 03/06/2024.
+//
+
+import XCTest
+
+final class NikeCloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
